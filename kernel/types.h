00d976dd8748365b17a92972ba601e201e800ff5
lhcmaple <36978110+lhcmaple@users.noreply.github.com>
2019-06-11 23:54:00 +0800

    Update types.h

--- kernel/types.h
+++ kernel/types.h
@@ -3,3 +3,12 @@
 #define NULL ((void *)0)
 
 typedef void (*TaskFUNC)(void *);
+
+struct TaskTCB{
+	char *stack_top;//进程堆栈保存处
+	struct TaskTCB *next;//下一任务
+	char stack[STACK_SIZE];//进程堆栈
+	int pid;//进程ID
+	TaskFUNC task_func;//初始化任务函数
+	void *pvalue;//初始化参数
+};
