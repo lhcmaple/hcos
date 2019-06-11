c1567702fa9f4265683295445b2386372d5af608
lhcmaple <36978110+lhcmaple@users.noreply.github.com>
2019-06-11 23:51:34 +0800

    Update sched.h

--- kernel/sched.h
+++ kernel/sched.h
@@ -1,7 +1,5 @@
 #include "types.h"
 
-extern struct TaskTCB;
-
 void initsystem(void);
-void addtasktolist(struct TaskTCB *,void (*)(void *),void *);
+void addtasktolist(struct TaskTCB *,TaskFUNC,void *);
 void startsystem(void);
